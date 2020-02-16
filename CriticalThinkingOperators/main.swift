469841ad4659cb7278495cbc7f0043d06badb1dc
skrall3119 <alexjanci@yahoo.com>
2020-02-16 15:25:08 -0600

    Initial Commit

---
+++ CriticalThinkingOperators/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CriticalThinkingOperators
+//
+//  Created by Alex Janci on 2/16/20.
+//  Copyright © 2020 Alex Janci. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
