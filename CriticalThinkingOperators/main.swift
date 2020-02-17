3ea1fb6ecd6e72349aa3ae685993508c552dc669
skrall3119 <alexjanci@yahoo.com>
2020-02-17 00:03:41 -0600

    initial commit

--- CriticalThinkingOperators/main.swift
+++ CriticalThinkingOperators/main.swift
@@ -8,5 +8,21 @@
 
 import Foundation
 
-print("Hello, World!")
+let firstTime = "10:30AM"
+let secondTime = "11:54PM"
+
+let formatter = DateFormatter()
+formatter.dateFormat = "h:mma"
+
+let firstDate = formatter.date(from: firstTime)!
+let secondDate = formatter.date(from: secondTime)!
+
+let elapsedTimeInSeconds = secondDate.timeIntervalSince(firstDate)
+let hours = floor(elapsedTimeInSeconds / 60 / 60)
+let minutes = floor((elapsedTimeInSeconds - (hours * 60 * 60)) / 60)
+
+
+print("From the time \(firstTime) to \(secondTime), \(elapsedTimeInSeconds) seconds have passed.")
+print("this simplifies to about \(hours) hours and \(minutes) minutes")
+
 
